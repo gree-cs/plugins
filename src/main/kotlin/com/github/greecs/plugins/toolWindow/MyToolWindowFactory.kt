b8faaa0c4d00d4540b2390e1251d28fc6cb1827f
GitHub Action <action@github.com>
2024-10-20 05:02:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/greecs/plugins/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/greecs/plugins/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.greecs.plugins.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.greecs.plugins.MyBundle
+import com.github.greecs.plugins.services.MyProjectService
 import javax.swing.JButton
 
 
