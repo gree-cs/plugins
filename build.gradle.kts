61f8350babd1c3e20eb49f0f600b9474acf2cd43
weixin_43150621 <hyj_code@163.com>
2024-10-20 19:40:25 +0800

    初始化

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import groovy.xml.dom.DOMCategory.attributes
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
@@ -5,6 +6,7 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 plugins {
     id("java") // Java support
     alias(libs.plugins.kotlin) // Kotlin support
+
     alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
@@ -33,10 +35,29 @@ repositories {
 dependencies {
     testImplementation(libs.junit)
 
+    // 运行时依赖
+// https://mvnrepository.com/artifact/net.bytebuddy/byte-buddy
+    implementation("net.bytebuddy:byte-buddy:1.12.6")
+// https://mvnrepository.com/artifact/net.bytebuddy/byte-buddy-agent
+    testImplementation("net.bytebuddy:byte-buddy-agent:1.12.6")
+
+// https://mvnrepository.com/artifact/cn.hutool/hutool-all
+    implementation("cn.hutool:hutool-all:5.8.32")
+
+// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.53")
+
+// https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    implementation("com.mysql:mysql-connector-j:8.3.0")
+
+// https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.2")
+
+
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
-
+        //create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+        local("D:\\software\\IntelliJ IDEA 2024.1")
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
 
@@ -101,11 +122,11 @@ intellijPlatform {
         channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
 
-    pluginVerification {
+   /* pluginVerification {
         ides {
             recommended()
         }
-    }
+    }*/
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
@@ -129,7 +150,16 @@ tasks {
     wrapper {
         gradleVersion = providers.gradleProperty("gradleVersion").get()
     }
-
+    withType<JavaCompile> {
+   /*     sourceCompatibility = "17"
+        targetCompatibility = "17"*/
+        // 解决编译时中文报错
+        options.encoding = "UTF-8"
+    }
+    // 添加以下内容，解决运行时控制台中文乱码
+    withType<JavaExec> {
+        jvmArgs = listOf("-Dfile.encoding=UTF-8", "-Dsun.stdout.encoding=UTF-8", "-Dsun.stderr.encoding=UTF-8")
+    }
     publishPlugin {
         dependsOn(patchChangelog)
     }
---
+++ src/main/java/com/github/greecs/plugins/PerRun.java
@@ -0,0 +1,54 @@
+package com.github.greecs.plugins;
+
+import cn.hutool.core.util.StrUtil;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.JavaParameters;
+import com.intellij.execution.configurations.ParametersList;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.runners.JavaProgramPatcher;
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.JavaSdkVersion;
+import com.intellij.openapi.projectRoots.Sdk;
+
+import java.util.Objects;
+
+public class PerRun extends JavaProgramPatcher {
+
+    @Override
+    public void patchJavaParameters(Executor executor, RunProfile configuration, JavaParameters javaParameters) {
+
+        if (!(configuration instanceof RunConfiguration)) {
+            return;
+        }
+
+        Sdk jdk = javaParameters.getJdk();
+
+        if (Objects.isNull(jdk)) {
+            return;
+        }
+
+        JavaSdkVersion version = JavaSdk.getInstance().getVersion(jdk);
+
+        if (Objects.isNull(version)) {
+            return;
+        }
+
+        if (version.compareTo(JavaSdkVersion.JDK_1_8) < 0) {
+            return;
+        }
+
+        String agentCoreJarPath = PluginUtil.getAgentCoreJarPath();
+
+        if (StrUtil.isBlank(agentCoreJarPath)) {
+            return;
+        }
+
+        RunConfiguration runConfiguration = (RunConfiguration) configuration;
+        ParametersList vmParametersList = javaParameters.getVMParametersList();
+        vmParametersList.addParametersString("-javaagent:" + agentCoreJarPath);
+        vmParametersList.addNotEmptyProperty("plugins.projectId", runConfiguration.getProject().getLocationHash());
+
+    }
+
+}
